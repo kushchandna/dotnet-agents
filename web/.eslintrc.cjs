2b1d8c7e956bf17477264f405b3f327d3c357e28
Kush <kushchandna@gmail.com>
2026-05-15 20:57:17 +1000

    add isolatedmodules and react-hooks eslint rules to web config

--- web/.eslintrc.cjs
+++ web/.eslintrc.cjs
@@ -2,7 +2,7 @@ module.exports = {
   root: true,
   parser: '@typescript-eslint/parser',
   plugins: ['@typescript-eslint', 'react', 'react-hooks'],
-  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'plugin:react-hooks/recommended'],
   settings: { react: { version: 'detect' } },
   rules: { 'react/react-in-jsx-scope': 'off' }
 };
